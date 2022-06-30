dde1d55630b291a3203b2c4953cb24758946d832
Bianchi Raffaele <BianchiRaffaele@libero.it>
2022-06-30 09:25:15 +0000

    ggwp

--- PROVA/polls/admin.py
+++ PROVA/polls/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import The_lord_of_ring,Author,Harry_Potter,The_Chronicles_of_Narnia,Star_War,Hunger_Game
+
+
+admin.site.register(Author)
+admin.site.register(Harry_Potter)
+admin.site.register(The_lord_of_ring)
+admin.site.register(The_Chronicles_of_Narnia)
+admin.site.register(Star_War)
+admin.site.register(Hunger_Game)
--- PROVA/polls/models.py
+++ PROVA/polls/models.py
@@ -1,3 +1,53 @@
+from ast import In
+from asyncore import read, write
+import filecmp
+import fileinput
+from importlib.resources import open_text
+from ntpath import altsep
+from optparse import TitledHelpFormatter
+from statistics import mode
+from tkinter import image_names
+from typing import Any, Generic
+from unittest.util import _MAX_LENGTH
+from urllib import response
 from django.db import models
+from django.forms import TextInput
 
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=20 )
+
+class Harry_Potter(models.Model):
+    Author = models.ForeignKey(Author,on_delete=models.CASCADE)
+    Title = models.CharField(max_length=50)
+    pub_date = models.DateField('date published')
+    
+ 
+
+class The_lord_of_ring(models.Model):
+    Author = models.ForeignKey(Author,on_delete=models.CASCADE)
+    Title = models.CharField(max_length=40)
+    pub_date = models.DateField('date published')
+
+
+
+class The_Chronicles_of_Narnia(models.Model):
+ Author = models.ForeignKey(Author,on_delete=models.CASCADE)
+ Title = models.CharField(max_length=40)
+ pub_date = models.DateField('date published')
+
+
+
+class Star_War(models.Model):
+    Author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    Title = models.CharField(max_length=60)
+    pub_date = models.DateField('date published')
+
+
+
+class Hunger_Game(models.Model):
+    Author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    Title = models.CharField(max_length=65)
+    pub_date = models.DateField(' date published')
+
